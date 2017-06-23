336962f36c911ad47b7cd38cd44f467fb3c7caee
Zachary Ehren <zachehren@gmail.com>
2017-06-22 20:07:34 -0700

    user authentication complete

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,9 @@
-class ApiController < ApplicationController
-  skip_before_action :verify_authenticity_token
+include ActionController::HttpAuthentication::Basic::ControllerMethods
+include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  respond_to :json
+class ApiController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+
 
   def authenticated?
     authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }

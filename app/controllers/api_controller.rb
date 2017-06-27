7f6a94f0236190b1a752c53ee169699cdc36fbff
Zachary Ehren <zachehren@gmail.com>
2017-06-26 19:17:43 -0700

    command line checkpoint complete

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,8 +2,6 @@ include ActionController::HttpAuthentication::Basic::ControllerMethods
 include ActionController::HttpAuthentication::Token::ControllerMethods
 
 class ApiController < ApplicationController
-  # skip_before_action :verify_authenticity_token
-
 
   def authenticated?
     authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
      resources :users do
        resources :lists
      end
-   end
+
 
    resources :lists, only: [] do
      resources :items, only: [:create]
    end
 
    resources :items, only: [:destroy]
-   
+
+ end 
 end

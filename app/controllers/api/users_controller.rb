908b02184da9b0d59bfe755bf6f3e21fefb4f11b
Zachary Ehren <zachehren@gmail.com>
2017-06-28 15:20:11 -0700

    destroy lists & users complete

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,6 +17,16 @@ class Api::UsersController < ApiController
     end
   end
 
+  def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
+
   private
    def user_params
      params.require(:user).permit(:username, :password)

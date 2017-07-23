c201f8d72457e483f2e4199f5ad8334318fe9081
Zachary Ehren <zachehren@gmail.com>
2017-07-23 14:32:38 -0700

    add show action for User

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,6 +7,11 @@ class Api::UsersController < ApiController
     render json: @users, each_serializer: UserSerializer
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user, each_serializer: UserSerializer
+  end
+
   def create
     user = User.new(user_params)
      if user.save
